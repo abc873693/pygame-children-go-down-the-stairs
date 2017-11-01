39c2822fe675df9ce2408e6034f66e85686fefbf
1103105348 <fate010808@gmail.com>
2017-11-02 01:08:45 +0800

    add floor_number
    新增了層數的顯示

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import util
 import models
-import pygame,random
+import pygame,random,time
 import colors
 from pygame.locals import *
 
@@ -12,6 +12,12 @@ pygame.init()
 screen = pygame.display.set_mode((mWidth, mHeight), 0, 32)
 pygame.display.set_caption("Sprite 精靈類別測試：one cat")
 framerate = pygame.time.Clock()
+font = pygame.font.Font("fonts/msjh.ttf", 24)  
+
+counts=0# 初始化计时器
+COUNT = pygame.USEREVENT+1 # 自定义计时事件
+pygame.time.set_timer(COUNT,1500)# 每隔1.5秒发送一次自定义事件
+
 
 all_floor = pygame.sprite.Group()
 
@@ -33,7 +39,10 @@ for i in range(5):
 state_character = 0
 gameLoop = True
 while gameLoop:
+    
     for event in pygame.event.get():
+      if event.type == COUNT:
+            counts=counts+1
       if event.type == pygame.QUIT:
         gameLoop = False
       if (event.type == pygame.KEYDOWN):
@@ -57,10 +66,12 @@ while gameLoop:
     screen.fill(colors.black)
     character.update(state_character)
     character.draw(screen)
-    #group.update(state_character)
-    #group.draw(screen)
-    #floor.update(mWidth,mHeight)
-    #floor.draw(screen)
+   
+    text = "層數" 
+    util.showText(screen,font,text,360,15)
+    countstext=str(counts)
+    util.showText(screen,font,countstext,360,50)
+    
     
     # for item in group.sprites():
     #     if type(item) == models.Character:
--- util.py
+++ util.py
@@ -1,4 +1,17 @@
 
+import pygame,time
+import colors
+from pygame.locals import *
 
-def show(text):
-    print(text)
+
+# 定义几个颜色
+
+
+def showText(DISPLAYSURF,fontObj,text,x,y):#抽象出一个方法用来绘制Text在屏幕上
+
+    textSurfaceObj = fontObj.render(text, True, colors.green)# 配置要显示的文字
+    textRectObj = textSurfaceObj.get_rect()# 获得要显示的对象的rect
+    textRectObj.center = (x, y)# 设置显示对象的坐标
+
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)# 绘制字体
+
